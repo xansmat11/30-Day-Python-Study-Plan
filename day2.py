0cc334cfa0153345c8735d0d6cfaf5c46605e0e5
xansmat11 <257333934+xansmat11@users.noreply.github.com>
2026-07-19 22:25:49 +0800

    Add initial files

---
+++ day2.py
@@ -0,0 +1,23 @@
+# Day 2
+
+firstName = 'Matrix'
+lastName = 'Valde'
+fullName = 'Matrix Valde'
+country = 'Philippines'
+city = 'Bacolod'
+age = 22
+year = 2003
+isMarried = False
+isTrue = True
+isLightOn = True
+
+gfFirstName, gfLastName, country, age, isMarried = 'Trisha', 'Barbon', 'Philippines', 22, False
+
+print(len(firstName), len(lastName))
+
+numOne = 5
+numTwo = 4
+total = numOne + numOne
+diff = numOne - numTwo
+product = numOne * numTwo
+division = numOne / numTwo
---
+++ helloworld.py
@@ -0,0 +1,23 @@
+# Day 1 
+print(2 + 3)
+
+print(type({'name':'Matrix'}))
+
+planets = ('Earth', 'Mars', 'Jupiter', 'Saturn')
+print(*planets, sep=", ")
+
+# Eucledian Distance
+print(((10 - 2)**2 +(8 - 3)**2) ** 0.5)
+
+firstName = 'Matrix'
+lastName = 'Valde'
+country = 'Philippines'
+girlfriend = 'Trisha Barbon'
+
+print('My girlfriend is', girlfriend)
+
+print(len(country))
+
+print(int('10'))
+
+print(list(firstName))
