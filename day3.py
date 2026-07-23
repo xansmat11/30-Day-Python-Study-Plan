496f0d4de7db5cfddbd722da18373a25e967ef87
xansmat11 <257333934+xansmat11@users.noreply.github.com>
2026-07-23 11:12:32 +0800

    Made script to calculate the triangle perimeter

--- day3.py
+++ day3.py
@@ -1,4 +1,13 @@
+# Area of trianlge 
+
 base = int(input('Enter base: '))
 height = int(input('Enter height: '))
 area = 0.5 * base * height
 print('The area of the triangle is ', area)
+
+#Perimeter of triangle
+side_a = int(input('Enter side a: '))
+side_b = int(input('Enter side b: '))
+side_c = int(input('Enter side c: '))
+perimeter = side_a + side_b + side_c
+print('The perimeter of the triangle is', perimeter)
