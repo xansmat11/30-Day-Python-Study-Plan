285c1cdd870f258b9e257466a86e1b0685089802
xansmat11 <257333934+xansmat11@users.noreply.github.com>
2026-07-23 11:23:10 +0800

    Made scripts in both weekly earning and number of seconds live

--- day3.py
+++ day3.py
@@ -1,5 +1,4 @@
 # Area of trianlge 
-
 base = int(input('Enter base: '))
 height = int(input('Enter height: '))
 area = 0.5 * base * height
@@ -11,3 +10,14 @@ side_b = int(input('Enter side b: '))
 side_c = int(input('Enter side c: '))
 perimeter = side_a + side_b + side_c
 print('The perimeter of the triangle is', perimeter)
+
+#Weekly earning
+hours = int(input('Enter hours: '))
+raterPerHours = int(input('Enter rate per hours: '))
+weeklyEarning = hours * raterPerHours
+print('Your weekly earning is', weeklyEarning)
+
+#Number of seconds live
+numOfYears = int(input('Enter number of years you have lived: '))
+totalSeconds = numOfYears * 31536000
+print('You have live for', totalSeconds, 'seconds')
